8481375a710a093d9f0de8f090fb83a97ac08aa4
Vincent-Luca <92523860+Vincent-Luca@users.noreply.github.com>
2023-05-25 22:59:19 +0200

    First step

---
+++ Server/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Messing_Service | Server";
+
+            Console.WriteLine($"Starting server application now (v{ServerMain.Version})");
+            ServerMain main = ServerMain.CreateOrGetInstance();
+            main.Start();
+        }
+    }
+}
