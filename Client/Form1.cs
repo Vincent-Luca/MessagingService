5d2497123f71029fc4fa943262a940e3b4db2810
Vincent-Luca <92523860+Vincent-Luca@users.noreply.github.com>
2023-06-06 16:07:56 +0200

    hgf

--- Client/Form1.cs
+++ Client/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleTCP;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,33 @@ namespace Client
 {
     public partial class Form1 : Form
     {
+        SimpleTcpClient client = new SimpleTcpClient();
         public Form1()
         {
             InitializeComponent();
+            
+            client.StringEncoder = Encoding.UTF8;
+
+            int serverPort = 8888; // Replace with the server port
+            string serverIP = "127.0.0.1"; // Replace with the server IP address
+
+            client.Connect(serverIP, serverPort);
+
+            MessageBox.Show("Connected to server at " + serverIP + ":" + serverPort);
+
+            client.DataReceived += Client_DataReceived;
+
+            
+        }
+        static void Client_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            var receivedMessage = e.MessageString;
+            MessageBox.Show("Received message: " + receivedMessage);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            client.WriteLine(textBox1.Text);
         }
     }
 }
--- Server/ServerMain.cs
+++ Server/ServerMain.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.Remoting.Channels;
 using System.Security.Policy;
 using System.Text;
@@ -20,18 +21,37 @@ namespace Server
 
         public SimpleTcpServer Server => _server;
 
+        private static Dictionary<string, SimpleTcpClient> _clients;
+        public static Dictionary<string, SimpleTcpClient> clients => _clients;
+
 
         public ServerMain()
         {
             _server = new SimpleTcpServer();
 
-            
+            _clients = new Dictionary<string, SimpleTcpClient>();
+
+            _server.ClientConnected += Server_ClientConnected;
+
+            _server.ClientDisconnected += Server_ClientConnected;
+
+            _server.DataReceived += Server_DataReceived;
+
+            _server.Delimiter = 0x13; // Message delimiter
+            _server.StringEncoder = Encoding.UTF8;
+
+            int port = 8888; // Replace with the desired server port
+            _server.Start(port);
+
+            Console.WriteLine("Server started. Listening for incoming connections on port " + port);
+            Console.ReadLine();
+
+            _server.Stop();
         }
 
         public void start()
         {
-            SetUpConDiscon();
-
+            
         }
 
         public void stop()
@@ -39,28 +59,60 @@ namespace Server
 
         }
 
-        public void SetUpConDiscon()
+        static void Server_DataReceived(object sender, Message e)
         {
-            _server.ClientConnected += (sender, e) => Console.WriteLine($"Client ({e.Client.RemoteEndPoint}) connected!");
+            var receivedMessage = e.MessageString;
+            Console.WriteLine("Received message: " + receivedMessage);
 
-            _server.ClientDisconnected += (sender, e) => Console.WriteLine($"Client ({e.Client.RemoteEndPoint}) disconnected!");
-        }
+            // Process and handle the received message
 
-        private void DataRecivied()
-        {
-            _server.DataReceived += (sender, e) =>
+            // Example: Sending a message to a specific client
+            if (receivedMessage.StartsWith("@"))
             {
-                var ep = e.TcpClient.Client.RemoteEndPoint;
-                var msg = Encoding.UTF8.GetString(e.Data);
-                Console.WriteLine($"Received message from {ep}: \"{msg}\"");
-                e.Reply(Encoding.UTF8.GetBytes("Hello back!"));
-            };
+                string[] parts = receivedMessage.Split(new[] { ' ' }, 2);
+                if (parts.Length == 2)
+                {
+                    string clientId = parts[0].Substring(1);
+                    string message = parts[1];
+
+                    if (clients.TryGetValue(clientId, out var client))
+                    {
+                        client.Write(message);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Client not found: " + clientId);
+                    }
+                }
+            }
+            else
+            {
+                // Echo the message back to all clients
+                foreach (var client in clients.Values)
+                {
+                    client.Write(receivedMessage);
+                }
+            }
         }
 
-
-        static void ClientConnected(object sender, ConnectedClient e)
+        static void Server_ClientConnected(object sender, System.Net.Sockets.TcpClient e)
         {
-            Console.WriteLine($"Client ({e.Client}) connected!");
+            Console.WriteLine("Client connected: " + e.Client.RemoteEndPoint);
+
+            // Create a new SimpleTcpClient for the connected client and add it to the clients dictionary
+            var client = new SimpleTcpClient();
+            client.StringEncoder = Encoding.UTF8;
+
+            clients.Add(e.Client.RemoteEndPoint.ToString(), client);
+
+        }
+
+        static void Server_ClientDisconnected(object sender, System.Net.Sockets.TcpClient e)
+        {
+            Console.WriteLine("Client disconnected: " + e.Client.RemoteEndPoint);
+
+            // Remove the SimpleTcpClient for the disconnected client from the clients dictionary
+            clients.Remove(e.Client.RemoteEndPoint.ToString());
         }
 
         public static ServerMain CreateOrGetInstance()
